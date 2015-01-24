fe6686c4720882224c69d4918f143e00ab6774fb
Daniel <Daniel@Daniel-PC>
2015-01-24 15:48:16 +0100

    Removed mongo scripts.

--- server/docker-resources/scripts/mongo/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MONGO_DATA_DIR="/docker-volumes/ldf-mongodb/data"
-MONGO_LOGS_DIR="/docker-volumes/ldf-mongodb/syslog"
-
-docker rm -f ldf-mongodb
-
-echo "Launching docker instance for mongodb."
-
-docker run -d -p 27017:27017 --name ldf-mongodb -v $MONGO_LOGS_DIR:/var/log -v $MONGO_DATA_DIR:/data cspinformatique/mongodb
