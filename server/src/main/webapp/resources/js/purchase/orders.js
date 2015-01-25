0a291c731d7189a5e146f7cd9a4d23ecdb431a75
dlavoie <dlavoie@cspinformatique.com>
2015-01-25 17:57:04 +0100

    Added total quantity to order screen.

--- server/src/main/webapp/resources/js/purchase/orders.js
+++ server/src/main/webapp/resources/js/purchase/orders.js
@@ -1,6 +1,17 @@
 var app = angular.module("KubikPurchaseOrders", []);
 
 app.controller("KubikPurchaseOrdersController", function($scope, $http){
+	$scope.calculateOrderQuantity = function(order){
+		var quantity = 0;
+		for(var detailIndex in order.details){
+			var detail = order.details[detailIndex];
+			
+			quantity += detail.quantity;
+		}
+		
+		return quantity;
+	};
+	
 	$scope.loadOrders = function(){
 		$http.get("purchaseOrder").success(function(orders){
 			$scope.orders = orders;
