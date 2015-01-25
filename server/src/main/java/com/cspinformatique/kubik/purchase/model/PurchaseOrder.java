aa0d11d68bd480dec5fe6153bb46b66632032423
dlavoie <dlavoie@cspinformatique.com>
2015-01-25 17:10:47 +0100

    Fixed purchase order id generation.

--- server/src/main/java/com/cspinformatique/kubik/purchase/model/PurchaseOrder.java
+++ server/src/main/java/com/cspinformatique/kubik/purchase/model/PurchaseOrder.java
@@ -7,8 +7,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -60,7 +58,6 @@ public class PurchaseOrder {
 	}
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
 	public long getId() {
 		return id;
 	}
