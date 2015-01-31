0fe88a567844113a6c4090b060e07d05b33f3d5e
Daniel Lavoie <dlavoie@live.ca>
2015-01-31 08:38:22 +0100

    Proxy print service now deletes the print job after completion.

--- proxy/src/main/java/com/cspinformatique/kubik/service/impl/PrintServiceImpl.java
+++ proxy/src/main/java/com/cspinformatique/kubik/service/impl/PrintServiceImpl.java
@@ -19,45 +19,54 @@ import com.cspinformatique.kubik.service.ServerService;
 @Service
 public class PrintServiceImpl implements PrintService, InitializingBean {
 	private static final DocFlavor PRINT_FLAVOR = DocFlavor.INPUT_STREAM.AUTOSENSE;
-	
-	@Autowired private ServerService serverService;
-	
+
+	@Autowired
+	private ServerService serverService;
+
 	private javax.print.PrintService printer;
 
 	@Value("${kubik.proxy.printer.name}")
 	private String printerName;
-	
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
-	    javax.print.PrintService[] services = PrintServiceLookup.lookupPrintServices(PRINT_FLAVOR, null);
-	    
-        for(javax.print.PrintService service : services) {
-            if(service.getName().contains(printerName)) {
-             	printer = service;
-                break;
-            }
-        }
-        
-        if(printer == null){
-        	throw new RuntimeException("Printer " + printerName + " could not be found.");
-        }
-	}
-	
-	@Override
-	public void executePrintJobs(){
-		for(ReceiptPrintJob receiptPrintJob : this.serverService.findPendingReceiptPrintJob()){
-			this.print(this.serverService.loadInvoiceReceiptData(receiptPrintJob.getInvoice()));
+		javax.print.PrintService[] services = PrintServiceLookup
+				.lookupPrintServices(PRINT_FLAVOR, null);
+
+		for (javax.print.PrintService service : services) {
+			if (service.getName().contains(printerName)) {
+				printer = service;
+				break;
+			}
+		}
+
+		if (printer == null) {
+			throw new RuntimeException("Printer " + printerName
+					+ " could not be found.");
 		}
 	}
-	
-	private void print(byte[] content) {
+
+	@Override
+	public void executePrintJobs() {
+		for (ReceiptPrintJob receiptPrintJob : this.serverService
+				.findPendingReceiptPrintJob()) {
+			this.print(receiptPrintJob);
+		}
+	}
+
+	private void print(ReceiptPrintJob receiptPrintJob) {
 		try {
 			PrinterJob job = PrinterJob.getPrinterJob();
-		    job.setPrintService(printer);
-		    
-		    PDDocument.load(new ByteArrayInputStream(content)).silentPrint(job);
+			job.setPrintService(printer);
+
+			PDDocument.load(
+					new ByteArrayInputStream(this.serverService
+							.loadInvoiceReceiptData(receiptPrintJob
+									.getInvoice()))).silentPrint(job);
 		} catch (Exception ex) {
 			throw new RuntimeException(ex);
+		} finally {
+			this.serverService.deleteReceiptPrintJob(receiptPrintJob.getId());
 		}
 	}
 }
