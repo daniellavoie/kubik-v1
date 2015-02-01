d25c9f5f53623a3e732dc9ee798b9841daf073ef
Daniel Lavoie <dlavoie@live.ca>
2015-02-01 20:40:12 +0100

    Added missing method on product repository.

--- server/src/main/java/com/cspinformatique/kubik/product/repository/ProductRepository.java
+++ server/src/main/java/com/cspinformatique/kubik/product/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.cspinformatique.kubik.product.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +13,9 @@ import com.cspinformatique.kubik.product.model.Supplier;
 
 public interface ProductRepository extends
 		PagingAndSortingRepository<Product, Integer> {
+	@Query("SELECT id FROM Product WHERE dilicomReference = ?")
+	List<Integer> findIdByDilicomReference(boolean dilicomReference);
+	
 	public Product findByEan13AndSupplier(String ean13, Supplier supplier);
 
 	@Query("SELECT product FROM Product product WHERE ean13 LIKE :query OR extendedLabel LIKE :query OR publisher LIKE :query OR collection LIKE :query OR author LIKE :query OR isbn LIKE :query")
