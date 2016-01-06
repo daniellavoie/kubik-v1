6c93ca5a670224d6fe06ccef5744c9a287c22095
Daniel <Daniel@Daniel-PC>
2016-01-06 11:47:44 +0100

    Removed account features from header.

--- kubik/kubik-server/src/main/java/com/cspinformatique/kubik/server/domain/product/service/ProductService.java
+++ kubik/kubik-server/src/main/java/com/cspinformatique/kubik/server/domain/product/service/ProductService.java
@@ -1,7 +1,6 @@
 package com.cspinformatique.kubik.server.domain.product.service;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
