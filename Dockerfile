7aaf57504f2fed4321cfaa41283c534c7ba0c791
fteychene <francois.teychene@gmail.com>
2016-04-23 18:36:28 +0200

    Modify build and docker compilation for software factory. Fix some problems on start code. Add actuator. Add exception for monitoring /health on SecurityGroup. Add docker-compose for tests. Add Dockerfile for compilation and compose for kubik.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM java:openjdk-8u77-jre-alpine
+
+MAINTAINER Daniel Lavoie <dlavoie@cspinformatique.com>
+
+ADD kubik/kubik-server/target/kubik.jar /kubik/kubik.jar
+
+VOLUME [/"data"]
+
+EXPOSE 8080
+
+CMD ["java", "-jar", "/kubik/kubik.jar"]
--- kubik/kubik-server/src/main/java/com/cspinformatique/kubik/server/config/SecurityConfig.java
+++ kubik/kubik-server/src/main/java/com/cspinformatique/kubik/server/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/css/**", "/img/**", "/js/**", "/libs/**").permitAll().anyRequest()
+		http.authorizeRequests().antMatchers("/css/**", "/img/**", "/js/**", "/libs/**", "/health").permitAll().anyRequest()
 				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().logoutUrl("/logout")
 				.permitAll().and().csrf().disable().httpBasic();
 	}
